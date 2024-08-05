d3dac17d428277686fe2b7344a1cef3808581d97
archansy <archansyogeesh@gmail.com>
2024-08-05 14:01:52 +0530

    My first message

---
+++ src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,16 @@
+package com.actitime.testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+
+
+public void createCustomer()
+{
+	Reporter.log("bangalore",true);
+	
+}
+}
